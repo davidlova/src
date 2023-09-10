89ef356e496fae3edcbd4c3b74a3427196d62221
@b#! <138832434+davidlova@users.noreply.github.com>
2023-09-10 09:45:45 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,12 +7,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 24490919
-API_HASH = "d1b3b15126c47dd4cb491553ee1db910"
-BOT_TOKEN = "6603677482:pfNBWkvqoa8guMOCCSwU7X4"
+API_ID = 8405475
+API_HASH = "2575cc32b15cc2002fc1903324217d62"
+BOT_TOKEN = "6302165581:AAEkmZAWucqjnkEtNSPbZY1AFHHB1FoDbxs"
 SESSION = "BQC6JkIkHLfGZpi8gnxgY_IyjkTBiKXNqRuVQyXG0bV_LRh_HFgj91EEkMU0sGlLC5T7N9NVzlfCfMG8025sqEwbJ8GoDLUV0eAegMvVFzmjNReePmgnoNwfRpeMFbMv4V26a-AZIGRUJsQr2S8VAmFoBY-zxvkv8u6Hs4a5p-P_PXyutZDPSNn-bfrrqVxpGCi8QpqQjPaCfo-xnDWExz4TWFIJ6X6mZ8s-kRCNvmrV0pGvLFojD4tqUAA54A2qI0dv04GlcPumYPiNCGQPCYQdFY4LiRHKPkjBHh0xWsRv7SN2pHrRxd1-_3o1QbZPYjUzyH_PavVAOAAAAAXTGvwYA"
-FORCESUB = "batchingupdate"
-AUTH = 6302251074
+FORCESUB = "sachme001"
+AUTH = 108380022
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
